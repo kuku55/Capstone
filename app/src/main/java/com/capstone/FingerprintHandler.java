563173c5d12fd1b49b8f315266e1d5621368f864
Azzih Salayon <salayonaa@gmail.com>
2019-09-24 21:19:12 +0800

    Updated Email Verif

--- app/src/main/java/com/capstone/FingerprintHandler.java
+++ app/src/main/java/com/capstone/FingerprintHandler.java
@@ -11,10 +11,19 @@ import android.os.CancellationSignal;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.capstone.AlertActivity;
+import com.capstone.json.MySingleton;
+import com.capstone.location.EmergencyLocation;
 import com.capstone.message.MessageContact;
 import com.capstone.message.MessageContactActivity;
 import com.example.dana.capstone.R;
@@ -26,8 +35,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
 
@@ -68,6 +82,8 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
     @Override
     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+
+
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -87,6 +103,18 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
                 String latValue = String.valueOf(location.getLatitude());
                 String longValue = String.valueOf(location.getLongitude());
 
+                String KEY_UID = "uID";
+                String KEY_NAME = "name";
+                String KEY_ADDRESS = "address";
+                String KEY_CITY = "city";
+                String KEY_STATE = "state";
+                String KEY_COUNTRY = "country";
+                String KEY_POSTAL = "postalCode";
+                String KEY_LATITUDE = "latitude";
+                String KEY_LONGITUDE = "longitude";
+                String KEY_SUBJECT = "subject";
+                String KEY_MESSAGE = "message";
+
                 auth = FirebaseAuth.getInstance();
                 databaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -101,14 +129,56 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
                 }
 
                 Date currentTime = Calendar.getInstance().getTime();
+                SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+                String presentDate = df.format(currentTime);
                 String messageID = currentTime + auth.getUid() + "2" + cName; //2 == to
                 String uid = auth.getUid();
                 String message = "Latitude: " + latValue + " Longitude: " + longValue;
                 String subject = "EMERGENCY";
                 String receiver = cName; //changeable to cID + cName
-                String dateTime = currentTime.toString().trim();
+                String isRead = "unread";
 
-                MessageContact mc = new MessageContact(messageID, uid, subject, message, receiver, dateTime);
+                if(cName == "Police"){
+                    EmergencyLocation el = (EmergencyLocation) ((Activity)context).getIntent().getSerializableExtra("EMERGENCY");
+
+                    JSONObject request = new JSONObject();
+                    try {
+                        //Populate the request parameters
+                        request.put(KEY_UID, uid);
+                        request.put(KEY_NAME, cName);
+                        request.put(KEY_MESSAGE, message);
+                        request.put(KEY_SUBJECT, subject);
+//                        request.put(KEY_ADDRESS, el.getAddress());
+//                        request.put(KEY_CITY, el.getCity());
+//                        request.put(KEY_STATE, el.getState());
+//                        request.put(KEY_COUNTRY, el.getCountry());
+//                        request.put(KEY_POSTAL, el.getPostalCode());
+//                        request.put(KEY_STATE, el.getState());
+//                        request.put(KEY_LATITUDE, el.getLatitude());
+//                        request.put(KEY_LONGITUDE, el.getLongitude());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    String register_url = "https://e-ligtas.000webhostapp.com/json/emergency.php";
+                    JsonObjectRequest jsArrayRequest = new JsonObjectRequest
+                            (Request.Method.POST, register_url, request, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    Toast.makeText(context.getApplicationContext(), "Emergency Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }, new Response.ErrorListener() {
+
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(context.getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                                }
+                            });
+                    // Access the RequestQueue through your singleton class.
+                    MySingleton.getInstance(context.getApplicationContext()).addToRequestQueue(jsArrayRequest);
+                }
+
+                MessageContact mc = new MessageContact(messageID, uid, subject, message, receiver, presentDate);
                 databaseReference.child("Messages").child(messageID).setValue(mc).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
